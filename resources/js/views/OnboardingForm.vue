52f2edc43cc0d4e311bca5f6faf8ff59bde339f1
Chris Rocha <CRocha@IECom.local>
2023-02-03 17:19:23 -0500

    Committer: Chris Rocha <CRocha@IECom.local>
    On branch master
    
    Initial commit
    
    Changes to be committed:
    	new file:   resources/js/views/OnboardingForm.vue
    
    Untracked files:
    	.editorconfig
    	.env.example
    	.gitattributes
    	.gitignore
    	README.md
    	app/
    	artisan
    	bootstrap/
    	composer.json
    	composer.lock
    	config/
    	database/
    	lang/
    	package-lock.json
    	package.json
    	phpunit.xml
    	postcss.config.js
    	public/
    	resources/App.vue
    	resources/assets/
    	resources/css/
    	resources/js/app.js
    	resources/js/axiosConfig.js
    	resources/js/bootstrap.js
    	resources/js/components/
    	resources/js/views/Home.vue
    	resources/js/views/NotFound.vue
    	resources/js/views/Register.vue
    	resources/js/views/ResetPassword.vue
    	resources/views/
    	routes/
    	storage/
    	tailwind.config.js
    	tests/
    	vite.config.js

---
+++ resources/js/views/OnboardingForm.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1>Onboarding Form</h1>
+    <button @click="logout">Logout</button>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+    name: "Onboarding From",
+    methods: {
+        async logout(){
+            await axios.get('/api/logout')
+            .then(response => {
+                localStorage.removeItem('tokens')
+                this.$router.push({name: 'Home'})
+                this.$alert({
+                    title: 'Logout',
+                    text: response.data.message,
+                    type: 'success'
+                })
+            })
+        }
+    }
+}
+</script>
